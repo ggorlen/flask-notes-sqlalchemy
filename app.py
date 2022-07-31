c029ce03de1a850a56302263068044afb5844d24
ggorlen <gsgorlen@gmail.com>
2022-07-30 22:44:16 -0700

    Clarify db_session

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from db import Session
+from db import Base, Session
 from models import Note
 
 app = Flask(__name__)
@@ -14,42 +14,42 @@ def remove_session(*args, **kwargs):
 
 @app.route("/api/notes", methods=["GET", "POST"])
 def notes():
-    session = Session()
+    db_session = Session()
 
     if request.method == "POST":
         if "content" in request.json:
-            session.add(Note(content=request.json["content"]))
-            session.commit()
+            db_session.add(Note(content=request.json["content"]))
+            db_session.commit()
             return "", 201
 
         return "", 422
 
-    return jsonify(session.query(Note).all())
+    return jsonify(db_session.query(Note).all())
 
 @app.route("/api/notes/<int:note_id>", methods=["GET", "PUT", "DELETE"])
 def note(note_id):
-    session = Session()
+    db_session = Session()
 
     if request.method == "PUT":
         if "content" not in request.json:
             return "", 422
-        elif note := session.query(Note).filter(Note.id == note_id):
+        elif note := db_session.query(Note).filter(Note.id == note_id):
             note.update(
                 {"content": request.json["content"]},
                 synchronize_session="evaluate"
             )
-            session.commit()
+            db_session.commit()
             return "", 200
 
         return "", 404
     elif request.method == "DELETE":
-        if note := session.query(Note).get(note_id):
-            session.delete(note)
-            session.commit()
+        if note := db_session.query(Note).get(note_id):
+            db_session.delete(note)
+            db_session.commit()
             return "", 200
 
         return "", 404
-    elif result := session.query(Note).get(note_id):
+    elif result := db_session.query(Note).get(note_id):
         return jsonify(result)
 
     return "", 404
@@ -59,5 +59,6 @@ def index():
     return "", 200
 
 if __name__ == "__main__":
+    Base.metadata.create_all(engine)
     app.run(debug=True)
 
